92b839f0da74881e6d31d336ea99b7cafa6cceca
Abdullah Al Mamun <amamun@u.rochester.edu>
2020-11-21 13:08:28 -0500

    divider in better state in build version

--- src/MiniDetail.js
+++ src/MiniDetail.js
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
       fontWeight: "bold",
       color: "white",
       lineHeight: 1.7,
-      fontSize: "1.10em"
+      fontSize: "1.12em"
   }
 }));
 
